d4601873ebc88bc8e184b72a4224f4bc09c05a76
Lukas Svoboda <printeastwoodcz@gmail.com>
2020-10-21 22:54:39 +0200

    fixes

--- lib/src/store/storage/persistant_store.dart
+++ lib/src/store/storage/persistant_store.dart
@@ -63,12 +63,14 @@ class PersistantStore extends CrudStore with ExtendCrudStore {
   @override
   Future<bool> seed(List<MapEntry<String, String>> items) async {
     await init();
-    if (items != null && items?.isNotEmpty == true) {
+    var saved = await getAll();
+    if (saved.isEmpty && items != null && items?.isNotEmpty == true) {
       for (var item in items) {
         await create(item.key, item.value);
       }
+      return true;
     }
-    return true;
+    return false;
   }
 
   @override
