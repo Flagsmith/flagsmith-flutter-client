92433466a93fe1793ff16e68eef9f562b6962183
Jack Walker <jack@foresightmobile.com>
2021-10-13 15:45:15 +0100

    Added analytics functionality for keeping track of whether flags are enabled or not.

--- lib/src/flagsmith_config.dart
+++ lib/src/flagsmith_config.dart
@@ -9,6 +9,7 @@ class FlagsmithConfig {
   final String flagsURI;
   final String identitiesURI;
   final String traitsURI;
+  final String analyticsURI;
 
   final int connectTimeout;
   final int receiveTimeout;
@@ -19,10 +20,12 @@ class FlagsmithConfig {
 
   final bool isDebug;
   final bool caches;
+  final bool enableAnalytics;
+  final int analyticsInterval;
 
   /// Flagsmith config initialization
   /// change only if you have self-hosted Flagsmith
-  /// [baseURI], [flagsURI], [identitiesURI], [traitsURI]
+  /// [baseURI], [flagsURI], [identitiesURI], [traitsURI], [analyticsURI]
   ///
   /// Connection settings timeouts in milliseconds
   /// [connectionTimeout], [connectTimeout], [receiveTimeout], [sendTimeout]
@@ -36,12 +39,17 @@ class FlagsmithConfig {
   /// for self hosted server without valid cert use [Proxy](https://github.com/flutterchina/dio/blob/ee4d55d6fdb3d0658246460fe59567645a287ce4/README.md#using-proxy)
   ///
   /// if you want to use caches [caches] to *true*
+  ///
+  /// Analytics is enabled by default, to disable analytics set [enableAnalytics] to *false*
+  ///
+  /// The [analyticsInterval] is how often the analytics will be pushed to the server in milliseconds, defaults to 10000 (10 seconds)
 
   const FlagsmithConfig({
     this.baseURI = 'https://api.flagsmith.com/api/v1/',
     this.flagsURI = 'flags/',
     this.identitiesURI = 'identities/',
     this.traitsURI = 'traits/',
+    this.analyticsURI = 'analytics/flags/',
     this.connectTimeout = 10000,
     this.receiveTimeout = 20000,
     this.sendTimeout = 20000,
@@ -49,6 +57,8 @@ class FlagsmithConfig {
     this.password = 'flagsmith_sdk_secure',
     this.isDebug = false,
     this.caches = false,
+    this.enableAnalytics = true,
+    this.analyticsInterval = 10000,
   });
 
   String get traitsBulkURI => '${traitsURI}bulk/';
