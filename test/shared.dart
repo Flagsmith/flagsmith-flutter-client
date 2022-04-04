da0db2432bda0e3d2eceacca63b4a03dee26a289
Jack Walker <jack@foresightmobile.com>
2022-04-04 09:59:51 +0100

    Obscure Google API key

--- test/shared.dart
+++ test/shared.dart
@@ -265,11 +265,11 @@ final fakeResponse = r'''[
             "name": "oauth_google",
             "created_date": "2020-03-28T21:04:51.557946Z",
             "description": "Google login key",
-            "initial_value": "{\n \"clientId\":\"232959427810-br6ltnrgouktp0ngsbs04o14ueb9rch0.apps.googleusercontent.com\",\n \"apiKey\":\"AIzaSyCnHuN-y6BIEAM5vTISXaz3X9GpEPSxWjo\"\n}",
+            "initial_value": "{\n \"clientId\":\"232959427810-br6ltnrgouktp0ngsbs04o14ueb9rch0.apps.googleusercontent.com\",\n \"apiKey\":\"AIza****HuN-y6B********SXaz3X9GpEP*****\"\n}",
             "default_enabled": false,
             "type": "STANDARD"
         },
-        "feature_state_value": "{\n \"clientId\":\"232959427810-br6ltnrgouktp0ngsbs04o14ueb9rch0.apps.googleusercontent.com\",\n \"apiKey\":\"AIzaSyCnHuN-y6BIEAM5vTISXaz3X9GpEPSxWjo\"\n}",
+        "feature_state_value": "{\n \"clientId\":\"232959427810-br6ltnrgouktp0ngsbs04o14ueb9rch0.apps.googleusercontent.com\",\n \"apiKey\":\"AIza****HuN-y6B********SXaz3X9GpEP*****\"\n}",
         "enabled": true,
         "environment": 23,
         "identity": null,
